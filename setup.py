e8154ca066df7ecbd02f605f902cb6af244a9d8e
Kevin Gamez <ks.gamez@uniandes.edu.co>
2023-06-10 15:44:16 -0500

    fix setup dil

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 
+
 setup(
-    name='textos',
+    name='SapaceInvaders',
     packages=find_packages(include=['textos']),
     version='0.1.0',
     description='My first Python library',
     author='Me',
     license='MIT',
-    install_requires=['stanza','numpy','pandas','nltk','langdetect','flair','scikit-learn==1.2.1','num2words','requests','joblib', 'newspaper3k', 'PyPDF2', 'python-docx'],
+    install_requires=['botocore==1.29.151','urllib3==1.26.16','stanza==1.5.0','pandas==2.0.2','nltk==3.8.1','flair==0.12.2','scikit-learn==1.2.1','num2words==0.5.12','requests==2.31.0','joblib', 'newspaper3k==0.2.8', 'PyPDF2==3.0.1', 'python-docx==0.8.11'],
+    include_package_data=True,
+    package_dir={'SapaceInvaders': 'src/textos'},
+    package_data={'textos': ['textos/pipeline.joblib']},
 )
