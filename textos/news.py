5f7c4b9136b0723bf7359723fa91945cb0135947
Kevin Gamez <ks.gamez@uniandes.edu.co>
2023-06-10 16:49:02 -0500

    fix news.py file

--- textos/news.py
+++ textos/news.py
@@ -40,9 +40,10 @@ def getTextPdf(text_path: str) -> str:
 
 
 def load_model() -> Pipeline:
-    filename = 'pipeline.joblib'  # Ubicación del archivo entregado
+    stream = pkg_resources.resource_stream(__name__, 'pipeline.joblib')
+    #filename_svm = './textos/pipeline.joblib'  # Ubicación del archivo entregado
     # Deserializar el objeto del archivo
-    with open(filename, 'rb') as f:
+    with open(stream, 'rb') as f:
         model = joblib.load(f)
     return model
 
