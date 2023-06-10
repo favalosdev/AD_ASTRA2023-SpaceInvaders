37cc412fdc81b7a8dded6fd41d23932f9807608d
Kevin Gamez <ks.gamez@uniandes.edu.co>
2023-06-10 15:11:35 -0500

    update setup file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='My first Python library',
     author='Me',
     license='MIT',
-    install_requires=['urllib3==1.26.16','stanza==1.5.0','pandas==2.0.2','nltk==3.8.1','flair==0.12.2','scikit-learn==1.2.1','num2words==0.5.12','requests==2.31.0','joblib', 'newspaper3k==0.2.8', 'PyPDF2==3.0.1', 'python-docx==0.8.11'],
+    install_requires=['botocore==1.29.151','urllib3==1.26.16','stanza==1.5.0','pandas==2.0.2','nltk==3.8.1','flair==0.12.2','scikit-learn==1.2.1','num2words==0.5.12','requests==2.31.0','joblib', 'newspaper3k==0.2.8', 'PyPDF2==3.0.1', 'python-docx==0.8.11'],
     include_package_data=True,
-    package_data={'': ['textos/*.joblib']},
+    package_data={'': ['./textos/pipeline.joblib']},
 )
